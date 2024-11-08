16aae9efed3aa404a503d9551defcd1b98a530f7
Chen-da-x <84442753+Chen-da-x@users.noreply.github.com>
2024-11-08 23:29:18 +0800

    Update kmeans.scala

--- kmeans.scala
+++ kmeans.scala
@@ -1,54 +1,48 @@
 import scala.io.Source
 import scala.util.Random
 
-def distance(p:Vector[Double], q:Vector[Double]) : Double = {
-    math.sqrt(p.zip(q).map { case (pi, qi) => math.pow(pi-qi, 2)}.sum)
-}
- 
-def clostestpoint(q: Vector[Double], candidates: Array[Vector[Double]]): Vector[Double] = {
-	candidates.minBy(candidate => distance(q, candidate))
-}
- 
-def add_vec(v1: Vector[Double], v2: Vector[Double]): Vector[Double] = {
-	v1.zip(v2).map { case (x1, x2) => x1 + x2 }
-}
- 
-def average(cluster: Iterable[Vector[Double]]): Vector[Double] = {
-    val sumVector = cluster.reduce(add_vec)
+object KMeansClustering {
+  // Helper functions
+  def distance(p: Vector[Double], q: Vector[Double]): Double = {
+    math.sqrt(p.zip(q).map { case (pi, qi) => math.pow(pi - qi, 2) }.sum)
+  }
+
+  def closestPoint(q: Vector[Double], candidates: Array[Vector[Double]]): Vector[Double] = {
+    candidates.minBy(candidate => distance(q, candidate))
+  }
+
+  def addVec(v1: Vector[Double], v2: Vector[Double]): Vector[Double] = {
+    v1.zip(v2).map { case (x1, x2) => x1 + x2 }
+  }
+
+  def average(cluster: Iterable[Vector[Double]]): Vector[Double] = {
+    val sumVector = cluster.reduce(addVec)
     val count = cluster.size
     sumVector.map(_ / count)
-}
+  }
 
-def readDataPointsFromFile(filename: String): Array[Vector[Double]] = {
-  Source.fromFile(filename)
-    .getLines()
-    .map { line =>
-      line.split("\t").map(_.toDouble).toVector
-    }.toArray
-}
+  def readDataPointsFromFile(filename: String): Array[Vector[Double]] = {
+    Source.fromFile(filename)
+      .getLines()
+      .map(_.split("\t").map(_.toDouble).toVector)
+      .toArray
+  }
 
-def choose_random_centroids(data: Array[Vector[Double]], k: Int): Array[Vector[Double]] = {
-  Random.shuffle(data.toList).take(k).toArray
-}
+  def chooseRandomCentroids(data: Array[Vector[Double]], k: Int): Array[Vector[Double]] = {
+    Random.shuffle(data.toList).take(k).toArray
+  }
 
-////////////////////////
-// Main Program Below //
-////////////////////////
-
-object KMeansClustering {
+  // Main program
   def main(args: Array[String]): Unit = {
-    // Sample data: each vector is a point in the dataset
     val data = readDataPointsFromFile("clustering_dataset.txt")
-
-    // Initial centroids: randomly chosen or predefined
-    var centroids = choose_random_centroids(data, 3)
+    var centroids = chooseRandomCentroids(data, 3)
     var prevCentroids = Array[Vector[Double]]()
     val maxIterations = 100
     var hasConverged = false
-    val epsilon = 0.001 
+    val epsilon = 0.001
 
     for (iteration <- 1 to maxIterations if !hasConverged) {
-      val clusters = data.groupBy(clostestpoint(_, centroids))
+      val clusters = data.groupBy(closestPoint(_, centroids))
       prevCentroids = centroids
       centroids = clusters.map { case (_, vectors) => average(vectors) }.toArray
 
@@ -66,5 +60,3 @@ object KMeansClustering {
     centroids.foreach(centroid => println(s"Centroid: ${centroid.mkString(", ")}"))
   }
 }
-
-//Write something here.
